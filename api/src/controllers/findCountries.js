026baac37b37b154f577914b6188bb09aa294eca
MaxyGrosman <maxygrosman@gmail.com>
2023-04-28 15:27:51 -0300

    Updated findCountries controller

--- api/src/controllers/findCountries.js
+++ api/src/controllers/findCountries.js
@@ -1,8 +1,8 @@
 const {Country} = require('../db');
 const {Op} = require('sequelize');
 
-const findCountries = async(name) => Country.findAll({
-    where: {name: {[Op.substring]: name}}
+const findCountries = async(name) => await Country.findAll({
+    where: {name: {[Op.iLike]: `%${name}%`}}
 });
 
 module.exports = findCountries;
